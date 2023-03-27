9ea8f810a86265e15332d5bcad0aeef868e0a2c5
Delpha05 <devlinsaha@gmail.com>
2023-03-27 16:10:16 +0530

    added hashing for password

--- Routes/UserRoute/UserRoute.js
+++ Routes/UserRoute/UserRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const shortid = require('shortid');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 const users = require("../../Models/User");
@@ -25,6 +26,8 @@ router.get('/getUser', async (req, res) => {
     }
 })
 
+const salt = 10;
+
 router.post('/register', async (req, res) => {
     try {
         const {name, age, gender, phone, email, password } = req.body;
@@ -32,25 +35,39 @@ router.post('/register', async (req, res) => {
         if (!name ||!age ||!gender ||!phone ||!email ||!password) {
             res.status(400).send({err: "Please enter all fields."});
         } else {
-            const newUser = new users({
-                _id: shortid.generate(),
-                name: name,
-                age: age,
-                gender: gender,
-                phone: phone,
-                email: email,
-                password: password               
-                
-            });
+            const user = await users.findOne({ email: email });
+
+            if(user){
+                res.status(400).send({err: "User already exists."});
+            } else {
+                bcrypt.hash(password, salt, async (err, hash) => {
+                    if(err){
+                        res.status(500).send({err: err});
+                    } else{
+                        const newUser = new users({
+                            _id: shortid.generate(),
+                            name: name,
+                            age: age,
+                            gender: gender,
+                            phone: phone,
+                            email: email,
+                            password: hash               
+                            
+                        });
+                        console.log(password);
 
             await newUser.save();
 
             res.status(200).send({message: "User created successfully."});
         }
+    })
+}
+}
     } catch (error) {
         res.status(400).send({err: error})
     }
 })
+
 
 router.get('/login', async (req, res) => {
     try {
@@ -64,14 +81,17 @@ router.get('/login', async (req, res) => {
             if (!user) {
                 res.status(404).send({err: 'Wrong Credentials'});
             } else {
-                if (user.password === password) {
+                bcrypt.compare(password, user.password, async (err, match) => {
+                    if(err){
+                        res.status(400).send({err: err.message});
+                    } else if (match) {
                     res.status(200).send({message: "Login Successfull"});
-                } else {
+                } else if (!match) {
                     res.status(404).send({err: 'Incorrect password'});
                 }
-            }
+            })
         }
-
+    }
     } catch (error) {
         res.status(400).send({err: "Something went wrong please try again"})
     }
